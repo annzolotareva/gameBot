9e9a9c75865963acf876ad093c777b39289e6672
Anna Zolotareva <anazolotarewa@yandex.ru>
2021-12-05 19:59:33 +0400

    переделала задание шестого урока

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 "use strict";
 
-let userEnteredNumber;
-const randomHiddenNumber = Math.floor(Math.random() * 100) + 1;
-
 const isNumber = function (num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
 const guessNumber = function () {
-  userEnteredNumber = +prompt("Угадай число от 1 до 100");
-  const two = function () {
-    if (userEnteredNumber === 0) {
+  const randomHiddenNumber = Math.floor(Math.random() * 100) + 1;
+
+  const repeat = function () {
+    let userEnteredNumber = prompt("Угадай число от 1 до 100");
+    if (isNumber(userEnteredNumber)) {
+      if (userEnteredNumber < randomHiddenNumber) {
+        alert("Загаданное число больше");
+        repeat();
+      } else if (userEnteredNumber > randomHiddenNumber) {
+        alert("Загаданное число меньше");
+        repeat();
+      } else {
+        alert("Поздравляю, Вы угадали!!!");
+      }
+    } else if (userEnteredNumber === null) {
       alert("Игра окончена");
-    } else if (!isNumber(userEnteredNumber)) {
-      alert("Введи число!");
-      guessNumber();
-    } else if (userEnteredNumber > randomHiddenNumber) {
-      alert("Загаданное число меньше");
-      guessNumber();
-    } else if (userEnteredNumber < randomHiddenNumber) {
-      alert("Загаданное число больше");
-      guessNumber();
-    } else if (userEnteredNumber === randomHiddenNumber) {
-      alert("Поздравляю, Вы угадали!!!");
+    } else {
+      alert("Введите число!");
+      repeat();
     }
   };
-  two();
+
+  repeat();
 };
+
 guessNumber();
